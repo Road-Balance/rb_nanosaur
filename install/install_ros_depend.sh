b49f259a1d2eb29e511b6547824dd156dcd9a894
kimsooyoung <tge1375@naver.com>
2021-05-29 04:20:16 +0900

    Update : install file

---
+++ install/install_ros_depend.sh
@@ -0,0 +1,26 @@
+sudo apt-get update && \
+    apt-get install -y --no-install-recommends apt-utils && \
+    apt-get install -y libglew-dev glew-utils libgstreamer1.0-dev \
+    libgstreamer-plugins-base1.0-dev libglib2.0-dev \
+    nvidia-tensorrt && \
+    rm -rf /var/lib/apt/lists/*
+
+cd /opt
+sudo git clone https://github.com/dusty-nv/jetson-utils.git && \
+sudo mkdir -p jetson-utils/build && cd jetson-utils/build && \
+sudo cmake ../ && \
+sudo make -j$(nproc) && \
+sudo make install && \
+sudo ldconfig
+
+pip3 install wheel
+pip3 install -U wstool
+
+# mkdir -p ~/ros2_ws/src
+# cd ~/ros2_ws/src
+# cd ../
+# colcon build
+
+sudo apt-get update && \
+    apt-get install libjpeg-dev zlib1g-dev python3-pip -y && \
+    rm -rf /var/lib/apt/lists/*
